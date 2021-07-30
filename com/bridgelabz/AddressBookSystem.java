144cb0af42abbe29ca0b4ed3f18b9d550aab5120
Ganesh <ganeshgavhad9@gmail.com>
2021-07-30 15:39:12 +0530

    Welcome To Address Book System Program

---
+++ com/bridgelabz/AddressBookSystem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Wecome to Address Book Program");
+    }
+
+}
